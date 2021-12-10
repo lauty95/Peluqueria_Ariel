5c6a101ab12c6cdd7f1e19981adeb41741f5b80a
lauty95 <lautaroJ95@gmail.com>
2021-12-09 21:52:18 -0300

    next day

--- client/src/Components/AdminClients.jsx
+++ client/src/Components/AdminClients.jsx
@@ -59,19 +59,19 @@ function AdminClients(props) {
         })
     }
 
-
+console.log(registrados)
     useEffect(() => {
-        const dia = Number(fechaActual.split("-")[0])
-        let manana = fechaActual.split("-")
-        manana[0] = (dia + 1).toString()
-        const hora = new Date().getHours()
-        if (hora >= 20) {
-            axios.get(`/getClients/${manana.join().replaceAll(",", "-")}`)
-                .then(r => setRegistrados(r.data))
-        } else {
-            axios.get(`/getClients/${fechaActual}`)
-                .then(r => setRegistrados(r.data))
-        }
+        // const dia = Number(fechaActual.split("-")[0])
+        // let manana = fechaActual.split("-")
+        // manana[0] = (dia + 1).toString()
+        // const hora = new Date().getHours()
+        // if (hora >= 20) {
+        //     axios.get(`/getClients/${manana.join().replaceAll(",", "-")}`)
+        //         .then(r => setRegistrados(r.data))
+        // } else {
+        // }
+        axios.get(`/getClients/${fechaActual}`)
+            .then(r => setRegistrados(r.data))
         axios.get(`/mensajeWsp`)
             .then(r => setMensaje(r.data.mensaje))
         axios.get(`/precio`)
