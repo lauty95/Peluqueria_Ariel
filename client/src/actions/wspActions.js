d91818f8ba3324971134f227e6f549f20e310156
lauty95 <lautaroJ95@gmail.com>
2022-02-12 10:42:19 -0300

    ContactMe cambiado a Ariel

--- client/src/actions/wspActions.js
+++ client/src/actions/wspActions.js
@@ -1,16 +1,16 @@
-const phoneDefault = 5493492587791
+const phoneDefault = 3492322020
 
 export const contactMe = (phoneNumber, message, name) => {
     return function () {
         if (!message && !phoneNumber) {
             if (name) {
                 window.open(
-                    `https://wa.me/${phoneDefault}?text=Hola Lauty! Soy ${name}, me estoy contactando desde tu sitio web`,
+                    `https://wa.me/${phoneDefault}?text=Hola Ariel! Soy ${name}, me estoy contactando desde tu sitio web`,
                     '_blank'
                 );
             } else {
                 window.open(
-                    `https://wa.me/${phoneDefault}?text=Hola Lauty! Me contacto desde tu sitio web`,
+                    `https://wa.me/${phoneDefault}?text=Hola Ariel! Me contacto desde tu sitio web`,
                     '_blank'
                 );
             }
