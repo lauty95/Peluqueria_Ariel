5a8eddabfcce86da2c31b2d9a072a50fa38230ae
lauty95 <lautaroJ95@gmail.com>
2022-03-06 22:01:05 -0300

    push url

--- api/src/routes/notificaciones.js
+++ api/src/routes/notificaciones.js
@@ -86,7 +86,8 @@ router.post("/newClient", async (req, res) => {
         });
         const payload = JSON.stringify({
             title: 'Nuevo cliente',
-            message: `${nombre} ha sacadco turno para el ${dia} a las ${turno}hs`
+            message: `${nombre} ha sacadco turno para el ${dia} a las ${turno}hs`,
+            url: `https://wa.me/549${telefono}?text=${`*ARIEL LUQUE PELUQUERÍA DE CABALLEROS*  te recuerda el turno de *${turno}* y recordá que por razones de protocolo *el cliente no podrá asistir con acompañantes* salvo si es un niño que necesita el acompañamiento de sus padres. Desde ya muchas gracias por elegirme. *RECORDÁ SER PUNTUAL*.   *VALOR DEL CORTE $600, CORTE PROMOCIÓN $300*`}`,
         })
 
         const noti = await Push.findAll()
