e10c3e4ca4a756b42542e014e4c76b5ad3ccdf5d
lauty95 <lautaroJ95@gmail.com>
2022-03-06 21:45:18 -0300

    push url

--- client/public/worker.js
+++ client/public/worker.js
@@ -3,9 +3,6 @@ console.log('Service worker')
 self.addEventListener('push', e => {
     const data = e.data.json()
     self.registration.showNotification(data.title, {
-        body: data.message,
-        data: {
-            url: 'https://peluqueria-ariel.vercel.app/admin'
-        }
+        body: data.message
     })
 })
