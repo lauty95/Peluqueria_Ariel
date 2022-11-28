ea730a1233255f5acf4b80555996af7aa1103883
Lauty <38353324+lauty95@users.noreply.github.com>
2022-11-28 01:43:28 -0300

    qr ruta corregida

--- api/src/routes/notificaciones.js
+++ api/src/routes/notificaciones.js
@@ -6,24 +6,14 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const { Op } = require('sequelize');
 const qrcode = require('qrcode-terminal');
 
+let client;
+
 const horarios = [
     '9:00', '9:30', '10:00', '10:30', '11:00', '11:30',
     '12:00', '12:30', '13:00', '13:30', '14:00', '14:30',
     '15:00', '15:30', '16:00', '16:30', '17:00', '17:30',
     '18:00', '18:30', '19:00', '19:30', '20:00'
 ]
-
-let client;
-
-client = new Client({
-    puppeteer: {
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox'
-        ],
-        authStrategy: new LocalAuth()
-    }
-});
 
 
 function acomodarFecha(date) {
@@ -49,9 +39,20 @@ function devolverFecha(date) {
 }
 
 router.get('/qr', async (req, res) => {
+    client = new Client({
+        puppeteer: {
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ],
+            authStrategy: new LocalAuth()
+        }
+    });
+
     client.on('qr', (qr) => {
         res.send(qr)
     });
+
     client.on('ready', () => {
         console.log('Client is ready!');
     });
